a2e02def55e04f30f4d01877653dd91cbc739e33
Michael Robinson <mike@pagesofinterest.net>
2023-11-06 21:08:46 +1300

    Updated navbar to no longer use JS

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -28,7 +28,7 @@ setInterval(function() {
 
 function hasScrolled() {
     var st = $(this).scrollTop();
-    
+
     // Make sure they scroll more than delta
     if(Math.abs(lastScrollTop - st) <= delta)
         return;
@@ -36,20 +36,20 @@ function hasScrolled() {
     // If they scrolled down and are past the navbar, add class .nav-up.
     // This is necessary so you never see what is "behind" the navbar.
     if (st > lastScrollTop && st > navbarHeight){
-        // Scroll Down            
-        $('nav').removeClass('nav-down').addClass('nav-up'); 
+        // Scroll Down
+        $('nav').removeClass('nav-down').addClass('nav-up');
         $('.nav-up').css('top', - $('nav').outerHeight() + 'px');
-        
+
     } else {
         // Scroll Up
-        if(st + $(window).height() < $(document).height()) {               
+        if(st + $(window).height() < $(document).height()) {
             $('nav').removeClass('nav-up').addClass('nav-down');
-            $('.nav-up, .nav-down').css('top', '0px');             
+            $('.nav-up, .nav-down').css('top', '0px');
         }
     }
 
     lastScrollTop = st;
 }
-    
-    
+
+
 $('.site-content').css('margin-top', $('header').outerHeight() + 'px');
