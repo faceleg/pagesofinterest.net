00402936b8bbc1c92500004552e3bed60c913d0c
Michael Robinson <mike@pagesofinterest.net>
2023-11-10 19:50:49 +1300

    Removed jquery, fixes #3

--- _assets/js/theme.js
+++ _assets/js/theme.js
@@ -1,52 +1,68 @@
- // alertbar later
-$(document).scroll(function () {
-    var y = $(this).scrollTop();
-    if (y > 280) {
-        $('.alertbar').fadeIn();
-    } else {
-        $('.alertbar').fadeOut();
-    }
+//chat.openai.com/share/7ba3169c-e983-46d3-ae5c-670dab33141d
+
+/**
+ * Handles the visibility of the alertbar based on the scroll position.
+ * @param {Event} event - The scroll event.
+ */
+https: document.addEventListener("scroll", function (event) {
+  // Get the vertical scroll position
+  var y = window.scrollY || document.documentElement.scrollTop;
+  // Select the alertbar element
+  var alertbar = document.querySelector(".alertbar");
+
+  // Show or hide the alertbar based on scroll position
+  alertbar.style.display = y > 280 ? "block" : "none";
 });
 
+// Hide Header on scroll down
+let didScroll = false;
+let lastScrollTop = 0;
+const delta = 5;
+const navbar = document.querySelector("nav");
 
-// Hide Header on on scroll down
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $('nav').outerHeight();
-
-$(window).scroll(function(event){
-    didScroll = true;
+// Listen for scroll events
+window.addEventListener("scroll", function () {
+  // Set the didScroll flag to true when a scroll event occurs
+  didScroll = true;
 });
 
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
+// Check if the user has scrolled at intervals
+setInterval(function () {
+  // If the user has scrolled, call the hasScrolled function
+  if (didScroll) {
+    hasScrolled();
+    didScroll = false;
+  }
 }, 250);
 
+/**
+ * Handles the visibility of the header based on the scroll position.
+ */
 function hasScrolled() {
-    var st = $(this).scrollTop();
+  // Get the current scroll position
+  var st = window.scrollY || document.documentElement.scrollTop;
+  // Get the height of the viewport
+  const windowHeight = window.innerHeight;
+  // Get the total height of the document
+  const scrollHeight = document.documentElement.scrollHeight;
 
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
+  // Check if the scroll difference is greater than the delta value
+  if (Math.abs(lastScrollTop - st) <= delta) return;
 
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
-        // Scroll Down
-        $('nav').removeClass('nav-down').addClass('nav-up');
-        $('.nav-up').css('top', - $('nav').outerHeight() + 'px');
-
-    } else {
-        // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-            $('nav').removeClass('nav-up').addClass('nav-down');
-            $('.nav-up, .nav-down').css('top', '0px');
-        }
+  // If the user scrolled down and is past the navbar, hide the navbar
+  if (st > lastScrollTop && st > navbar.offsetHeight) {
+    navbar.classList.remove("nav-down");
+    navbar.classList.add("nav-up");
+    navbar.style.top = -navbar.offsetHeight + "px";
+  } else {
+    // If the user scrolled up or is at the top, show the navbar
+    if (st + windowHeight < scrollHeight) {
+      navbar.classList.remove("nav-up");
+      navbar.classList.add("nav-down");
+      navbar.style.top = "0px";
     }
+  }
 
-    lastScrollTop = st;
+  // Update the last scroll position
+  lastScrollTop = st;
 }
