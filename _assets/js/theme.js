0a262f7470f448b650eedba3acdc77eb971eb6ef
Michael Robinson <mike@pagesofinterest.net>
2023-11-18 18:11:12 +1300

    Add link svg on hover/on mobile

--- _assets/js/theme.js
+++ _assets/js/theme.js
@@ -69,3 +69,40 @@ function hasScrolled() {
   // Update the last scroll position
   lastScrollTop = st;
 }
+
+// Adapted from https://ben.balter.com/2014/03/13/pages-anchor-links/ with https://sl.bing.net/UY7c2w7Cdo
+
+/**
+ * This function adds a link icon to each header element that has an id attribute.
+ * The link icon is clickable and redirects to the same header element.
+ */
+/**
+ * This function adds a link icon to each header element that has an id attribute.
+ * The link icon is clickable and redirects to the same header element.
+ */
+// Select all header elements from h2 to h6
+var headers = document.querySelectorAll("h2, h3, h4, h5, h6");
+// Loop through each header element
+for (var i = 0; i < headers.length; i++) {
+  var header = headers[i];
+  // Get the id attribute of the header element
+  var id = header.getAttribute("id");
+  // If the header element has an id attribute
+  if (id) {
+    // Create a link element with the same id as the href attribute
+    var linkBefore = document.createElement("a");
+    linkBefore.classList.add("header-link", "header-link-before");
+    linkBefore.setAttribute("href", "#" + id);
+    linkBefore.innerHTML =
+      '<svg class="svg-link" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z"/></svg>';
+
+    var linkAfter = document.createElement("a");
+    linkAfter.classList.add("header-link", "header-link-after");
+    linkAfter.setAttribute("href", "#" + id);
+    linkAfter.innerHTML =
+      '<svg class="svg-link" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z"/></svg>';
+    // Prepend the link element to the header element
+    header.insertBefore(linkBefore, header.firstChild);
+    header.innerHTML += linkAfter.outerHTML;
+  }
+}
