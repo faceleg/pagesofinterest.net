16097dfc2cdaf13da2f0353be91a9964192d3643
Michael Robinson <mike@pagesofinterest.net>
2023-11-21 11:30:27 +1300

    Fixed eslint, restore semicolons

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,9 +1,10 @@
 // purgecss.config.js
 
+// eslint-disable-next-line no-undef
 module.exports = {
   // These are the files that Purgecss will search through
-  content: ['./docs/**/*.html'],
+  content: ["./docs/**/*.html"],
 
   // These are the stylesheets that will be subjected to the purge
-  css: ['./docs/**/*.css']
-}
+  css: ["./docs/**/*.css"]
+};
