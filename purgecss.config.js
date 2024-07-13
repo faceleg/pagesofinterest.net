e9c41592f2894f6d4d809cbad9b8d24a06696774
Michael Robinson <mike@pagesofinterest.net>
2024-07-13 17:09:43 +1200

    Updated purgecss

--- purgecss.config.js
+++ purgecss.config.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   // These are the files that Purgecss will search through
-  content: ["./docs/**/*.html"],
+  content: ["./_site/**/*.html"],
 
   // These are the stylesheets that will be subjected to the purge
-  css: ["./docs/**/*.css"]
+  css: ["./_site/assets/*.css"]
 };
