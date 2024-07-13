af5a8bd8cf220e69030bf4f088b92bf2ff3a7989
Michael Robinson <mike@pagesofinterest.net>
2024-07-13 18:49:27 +1200

    Tweak purgecss options

--- purgecss.config.js
+++ purgecss.config.js
@@ -5,5 +5,7 @@ module.exports = {
   content: ["./_site/**/*.html"],
 
   // These are the stylesheets that will be subjected to the purge
-  css: ["./_site/assets/*.css"]
+  css: ["./_site/assets/*.css"],
+  fontFace: true,
+  keyframes: true
 };
